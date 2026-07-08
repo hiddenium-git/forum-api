310f82711ff5d4a547de2234b35fbe3ed347448e
hiddenium <yousef.nazeri.9385802271@gmail.com>
2026-07-08 14:30:53 +0330

    created custom user models

---
+++ backend/apps/authentication/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        null=True,
+        blank=True,
+    )
+
+    bio = models.TextField(
+        blank=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
