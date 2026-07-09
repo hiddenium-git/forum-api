7fbdaf693889d63586d70b1977ebf3e1b2b6ed5c
hiddenium <yousef.nazeri.9385802271@gmail.com>
2026-07-09 17:11:33 +0330

    Implement user registration API

--- backend/apps/authentication/views.py
+++ backend/apps/authentication/views.py
@@ -8,6 +8,12 @@ from .serializers import RegisterSerializer
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
+        serializer = RegisterSerializer(data=request.data)
+
+        print(serializer.is_valid())
+        print(serializer.errors)
+
         serializer.is_valid(raise_exception=True)
+        serializer = RegisterSerializer(data=request.data)
         serializer.save()
         return Response({'message':'User created successfully'},status=status.HTTP_201_CREATED)
